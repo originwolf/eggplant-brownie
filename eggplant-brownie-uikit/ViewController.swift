7e7d36d723ff1d540b8a30ec35d2f577e6048ee3
Pedro Martins <pedrogmhead@gmail.com>
2021-03-04 04:54:14 -0300

    added data from textField

--- eggplant-brownie-uikit/ViewController.swift
+++ eggplant-brownie-uikit/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var felicidadeTextField: UITextField! //Force unwrap
+    //o swift n permite variáveis nulas, então o app n builda.
+    //utilizando a '!' você evita esse erro e pode buildar.
+    //ela indica que a variável terá um valor quando o app executar
+    //essa técnica possui suas implicações
 
     @IBAction func adicionar(){
-        let nome: String = "churros" // var nome: String > inferencia direta do tipo da váriavel - explicita
-        let felicidade = "5" //tipo da variável auto identificado pelo swift - implicita
+        let nome = nomeTextField.text
+        let felicidade = felicidadeTextField.text
+        
         print("comi: \(nome) e fiquei com felicidade: \(felicidade)")
     }
 
